75cb077f900fc1732fc2515e1eed9b06f4b62da9
nosanan <nosanan@alumni.uv.es>
2024-10-15 13:07:39 +0200

    perimetro rectangulo y pi= 3.14159

--- Perim.R
+++ Perim.R
@@ -2,5 +2,8 @@ PerimetroCuadrado <- function(L){
   return(4*L)
 }
 PerimetroCircunf <- function(r){
-  return(2*pi*r)
+  return(2* 3.14159*r)
+}
+PerimetroRect <- function(x,y){
+  return(2*x+2*y)
 }
